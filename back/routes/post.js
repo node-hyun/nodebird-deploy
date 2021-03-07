c921b59b22e7c681bc971dfa50bea8277f3e9067
hyunsokstar <hyunsokstar@gamil.com>
2021-03-07 20:50:51 +0900

    update for lambda

--- back/routes/post.js
+++ back/routes/post.js
@@ -20,13 +20,13 @@ try {
 AWS.config.update({
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    region: 'ap-northeast-2',
+    region: 'us-east-2',
 });
 
 const upload = multer({
     storage: multerS3({
         s3: new AWS.S3(),
-        bucket: 'nodebird-hyun-s3',
+        bucket: 'nodebird-hyun2-s3',
         key(req, file, cb) {
             cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`)
         }
